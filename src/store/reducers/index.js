c27062b7094ba3989f9483511b79f801fd8af51a
jitinrathi910949 <30821612+jitinrathi910949@users.noreply.github.com>
2020-05-07 20:09:51 +0530

    Update index.js

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import { routerReducer } from "react-router-redux";
+import testReducer from './testReducer';
 
 export const rootReducers = combineReducers({
-  routing: routerReducer
+  routing: routerReducer,
+  testReducer
   });
